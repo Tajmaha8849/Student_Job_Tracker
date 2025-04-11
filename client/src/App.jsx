d05d83dcb4e3547e1f610d8df7e42994d9ac79f7
Shubham Prajapati <138375156+Tajmaha8849@users.noreply.github.com>
2025-04-11 18:02:58 +0530

    Update App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -9,7 +9,7 @@ function App() {
   const [statusFilter, setStatusFilter] = useState('All');
   const [sortOrder, setSortOrder] = useState('desc');
 
-  const API_URL = 'https://student-job-tracker-94z2.vercel.app/api';
+  const API_URL = 'https://student-job-tracker-3-0eow.onrender.com/api';
 
   useEffect(() => {
     fetchJobs();
